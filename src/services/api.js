9788eff6e7ea6db0d717af4307cc75517f0426a1
Nahit-1 <nahit86@outlook.com>
2019-06-27 12:52:06 +0100

    created signInForm page & corresponding api function

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,14 @@
 const baseUrl = 'http://localhost:3001'
 const signinUrl = baseUrl + '/signin'
 const validateUrl = baseUrl + '/validate'
+
+export function signin (username, password) {
+
+    return fetch(signinUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ username: username, password: password })						
+        }).then(resp => resp.json()) 
+}
+
+
+export default { signin }
