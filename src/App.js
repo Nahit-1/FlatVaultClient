f5b2d85ac23d5f74b9c1a33d88f041ae36e074ac
Nahit-1 <nahit86@outlook.com>
2019-06-27 12:29:13 +0100

    installed semantic ui react and semantic ui css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'semantic-ui-css/semantic.min.css'
+
 function App() {
   return (
     <div className="App">
